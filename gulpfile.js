06b0ae7b6693256051e9652c57fcd802f0dd27f1
Juan Diego Arriola Wundram <juandiegoaw333@gmail.com>
2020-09-08 18:01:05 -0600

    added minification of js

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,51 @@
 const { src, dest, series, parallel, watch } = require('gulp');
 const sass = require('gulp-sass'); 
-const browserSync = require('browser-sync');
+const uglify = require('gulp-uglify');
+const minify =require('gulp-minify-css');
+const browserSync = require('browser-sync').create();
 
 //constantes de trabajo
 const files = {
     scssPath: 'src/scss/**/*.scss',
+    javascriptPath: 'src/javascript/**/*.js',
     htmlPath: 'dist/**/*.html'
 }
-
+//funciones para la optimizacion de css y javascript
 function helloWorldTask(done) {
     console.log("Hello world! :D");
     done();
 }
 
-function scssTask(d) {
+function scssTask (d){
     return src(files.scssPath)
-        .pipe(sass())
-        .pipe(dest('dist/css'));
-}   
-
-function watchTask() {
-    watch(
-        [files.scssPath, files.htmlPath],
-        series(scssTask, reloadTask)
-    )
+    .pipe(sass())
+    .pipe(minify())
+    .pipe(dest('dist/css')); 
 }
-
-function serveTask(done) {
+function jsTask(d){
+    return src(files.javascriptPath)
+    .pipe(uglify())
+    .pipe(dest('dist/javascript')); 
+}
+function watchTask(){
+    watch(
+        [files.scssPath, files.htmlPath, files.javascriptPath],
+        series(scssTask, jsTask, reloadTask)
+        )
+}
+function serveTask(d){
     browserSync.init({
         server: {
-            baseDir: './dist/'
+            baseDir: './dist'
         }
-    })
-    done();
+    });
+    d();
 }
-
-function reloadTask(done) {
+function reloadTask(d){
     browserSync.reload();
-    done();
+    d();
 }
 
-exports.default = series(scssTask, serveTask, watchTask);
+exports.default = series(scssTask, jsTask, serveTask, watchTask);
+exports.css = scssTask;
+exports.js = jsTask;
